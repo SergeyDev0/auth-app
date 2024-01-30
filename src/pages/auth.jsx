7325af557fa1cc1b4e1f2ed16180e953795540df
sergey <makarkapar@gmail.com>
2024-01-30 22:22:58 +0300

    hhh

--- src/pages/auth.jsx
+++ src/pages/auth.jsx
@@ -1,31 +1,24 @@
-import React from "react"
+import React from "react";
 import { GoogleLogin } from "@react-oauth/google";
-
 const Auth = () => {
-  const responseGoogle = (response) => {
-    console.log(response.profileObj);
-    // Вывод данных профиля
-    console.log(response.profileObj.email);
-    console.log(response.profileObj.name);
-    console.log(response.profileObj.imageUrl);
-  };
   return (
     <>
-
-
-
-        <GoogleLogin
-          onSuccess={responseGoogle => {
-            console.log(responseGoogle);
-          }}
-          onError={() => {
-            console.log('Login Failed');
-          }}
-          scope="openid read write"
-          useOneTap
-        />;
+      <GoogleLogin
+        onSuccess={(responseGoogle) => {
+          console.log(responseGoogle);
+          console.log(response.profileObj.email);
+          console.log(response.profileObj.name);
+          console.log(response.profileObj.imageUrl);
+        }}
+        onError={() => {
+          console.log("Login Failed");
+        }}
+        scope="openid read write"
+        useOneTap
+      />
+      ;
     </>
-  )
+  );
 };
 
 export default Auth;
