e5307dfa46ff381ecc45b7778ebe6e7c08fab499
sergey <makarkapar@gmail.com>
2024-01-30 22:45:01 +0300

    test

--- src/pages/auth.jsx
+++ src/pages/auth.jsx
@@ -1,23 +1,32 @@
 import React from "react";
-import { GoogleLogin } from "@react-oauth/google";
-import { hasGrantedAllScopesGoogle } from '@react-oauth/google';
+import { GoogleLogin } from "react-google-login";
 
 const Auth = () => {
-  const hasAccess = hasGrantedAllScopesGoogle(
-    tokenResponse,
-    'google-scope-1',
-    'google-scope-2',
-  );
+  const responseGoogle = (response) => {
+    console.log(response.profileObj);
+    // Вывод данных в консоль профиля пользователя
+    console.log(response.profileObj.email);
+    console.log(response.profileObj.name);
+    console.log(response.profileObj.imageUrl);
+  };
+
+  const responseError = (response) => {
+    console.log("Login Failed", response);
+    // Обработка ошибки авторизации
+  };
+
   return (
-    <>
+    <div>
+      <h1>Авторизация через Google</h1>
       <GoogleLogin
         clientId="114098492060-6bek2p0f2544ht483hrrb1munpejn9nq.apps.googleusercontent.com"
         buttonText="Войти через Google"
         onSuccess={responseGoogle}
-        onFailure={responseGoogle}
+        onFailure={responseError}
         cookiePolicy={'single_host_origin'}
+        scope="https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email"
       />
-    </>
+    </div>
   );
 };
 
